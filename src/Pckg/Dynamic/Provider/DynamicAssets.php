d1085907450a27368589fdec7d43eca97ac29b0d
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-07 20:16:56 +0200

    Remove dependencies

--- src/Pckg/Dynamic/Provider/DynamicAssets.php
+++ src/Pckg/Dynamic/Provider/DynamicAssets.php
@@ -17,23 +17,10 @@ class DynamicAssets extends Provider
             'js/magnific.js',
             'css/magnific.css',
             /**
-             * Datatables
-             */
-            //'js/datatables.js',
-            //'css/datatables.css',
-            /**
              * Sort
              */
             'css/actions.css',
             'js/actions.js',
-            /**
-             * Externals
-             */
-            //'https://use.fontawesome.com/90d4cc6ef0.js',
-            //'https://cdn.datatables.net/t/bs/dt-1.10.11/datatables.min.css',
-            //'https://cdn.datatables.net/t/bs/dt-1.10.11/datatables.min.js',
-            //'https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.24/vue.min.js',
-            //'https://cdn.jsdelivr.net/vue.resource/0.9.0/vue-resource.min.js',
             /**
              * Help popover
              */
