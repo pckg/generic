9513d8064e6057528a7b093c380d68bfd0264a51
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-08 15:51:35 +0200

    Implement handle()

--- src/Pckg/Generic/Middleware/RegisterGenericAssets.php
+++ src/Pckg/Generic/Middleware/RegisterGenericAssets.php
@@ -2,7 +2,19 @@
 
 use Pckg\Framework\Provider;
 
+/**
+ * Override this class per project.
+ *
+ * Class RegisterGenericAssets
+ *
+ * @package Pckg\Generic\Middleware
+ */
+
 class RegisterGenericAssets extends Provider
 {
+
+    public function handle()
+    {
+    }
 
 }
