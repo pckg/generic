272c05177c0af13fbf5f3550cf39fc4065d091ba
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-02 00:27:06 +0100

    Fix

--- src/Pckg/Generic/Controller/Generic.php
+++ src/Pckg/Generic/Controller/Generic.php
@@ -55,7 +55,7 @@ class Generic
         $vars = $this->genericService->getVariables();
 
         return $route->layout
-            ? view($route->layout->template ?? 'Pckg\Generic:generic', $vars)
+            ? view($route->layout->template ?: 'Pckg\Generic:generic', $vars)
             : $vars;
     }
 
