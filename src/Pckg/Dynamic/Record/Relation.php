09b14400a7ea79c9b75087f88157eea5e90babf1
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-03 18:03:19 +0100

    Real fix

--- src/Pckg/Dynamic/Record/Relation.php
+++ src/Pckg/Dynamic/Record/Relation.php
@@ -3,7 +3,7 @@
 use Pckg\Database\Record as DatabaseRecord;
 use Pckg\Dynamic\Entity\Relations;
 
-class Relation extends Record
+class Relation extends DatabaseRecord
 {
 
     protected $entity = Relations::class;
