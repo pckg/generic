c759a18dca7166b8aad743e1c80f368de579979d
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-08 13:12:23 +0100

    Php fix

--- src/Pckg/Dynamic/Record/Func.php
+++ src/Pckg/Dynamic/Record/Func.php
@@ -1,9 +1,9 @@
 <?php namespace Pckg\Dynamic\Record;
 
-use Pckg\Database\Record;
+use Pckg\Database\Record as DatabaseRecord;
 use Pckg\Dynamic\Entity\Functions;
 
-class Func extends Record
+class Func extends DatabaseRecord
 {
 
     protected $entity = Functions::class;
