6970a3e6fab2f19f4df550a58cf657cbbfb52292
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-15 14:20:47 +0100

    Cleanup

--- src/Pckg/Dynamic/Resolver/TableView.php
+++ src/Pckg/Dynamic/Resolver/TableView.php
@@ -1,61 +1,30 @@
 <?php namespace Pckg\Dynamic\Resolver;
 
-use Impero\Apache\Record\Site;
-use Pckg\Concept\Reflect;
-use Pckg\Database\Record;
-use Pckg\Dynamic\Entity\Tables;
 use Pckg\Dynamic\Entity\TableViews;
 use Pckg\Dynamic\Service\Dynamic;
 use Pckg\Framework\Provider\RouteResolver;
-use Pckg\Framework\Request\Data\Session;
-use Pckg\Framework\Response;
 
 class TableView implements RouteResolver
 {
-
-    /**
-     * @var Router
-     */
-    protected $router;
-
-    /**
-     * @var Tables
-     */
-    protected $tableViews;
-
-    /**
-     * @var Response
-     */
-    protected $response;
-
-    /**
-     * @var Session
-     */
-    protected $session;
 
     /**
      * @var Dynamic
      */
     protected $dynamic;
 
-    public function __construct(TableViews $tableViews, Response $response, Dynamic $dynamic)
+    public function __construct(Dynamic $dynamic)
     {
-        $this->tableViews = $tableViews;
-        $this->response = $response;
         $this->dynamic = $dynamic;
     }
 
     public function resolve($value)
     {
-        //$this->dynamic->joinTranslationsIfTranslatable($this->tableViews);
-        //$this->dynamic->joinPermissionsIfPermissionable($this->tableViews);
-
-        return $this->tableViews->where('id', $value)
-                                ->oneOrFail(
-                                    function() {
-                                        $this->response->unauthorized('Table view not found');
-                                    }
-                                );
+        return (new TableViews())->where('id', $value)
+                                 ->oneOrFail(
+                                     function() {
+                                         response()->unauthorized('Table view not found');
+                                     }
+                                 );
     }
 
     public function parametrize($record)
