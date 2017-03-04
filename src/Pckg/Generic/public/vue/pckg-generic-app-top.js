b79340ff28d372df9ea8a086b9c6719792cd19e4
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-03-04 19:38:04 +0100

    Timeout, bootstrap

--- src/Pckg/Generic/public/vue/pckg-generic-app-top.js
+++ src/Pckg/Generic/public/vue/pckg-generic-app-top.js
@@ -14,12 +14,22 @@ var pckgDelimiters = {
 
 var pckgTimeout = {
     methods: {
-        timeout: function (name, callback, timeout) {
-            if (this['_pckgTimeout' + name]) {
-                clearTimeout(this['_pckgTimeout' + name]);
+        timeout: function (name, callback, timeout, object) {
+            if (typeof object == 'undefined') {
+                object = this;
             }
 
-            this['_pckgTimeout' + name] = setTimeout(callback, timeout);
+            this.removeTimeout(name, object);
+
+            this.setTimeout(name, callback, timeout, object);
+        },
+        removeTimeout: function (name, object) {
+            if (object['_pckgTimeout' + name]) {
+                clearTimeout(this['_pckgTimeout' + name]);
+            }
+        },
+        setTimeout: function (name, callback, timeout, object) {
+            object['_pckgTimeout' + name] = setTimeout(callback, timeout);
         }
     }
 };
