e9a5d3109eea9291666c035a6ffc52dda777254e
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-03-09 14:03:39 +0100

    Vue.js upgrade

--- src/Pckg/Maestro/public/js/pckg-htmlbuilder-dropzone.js
+++ src/Pckg/Maestro/public/js/pckg-htmlbuilder-dropzone.js
@@ -10,32 +10,33 @@ var pckgTabelizeFieldEditor = Vue.component('pckg-htmlbuilder-dropzone', {
             _dropzone: null
         };
     },
-    computed: {},
-    ready: function () {
-        if (!this.url) {
-            return;
-        }
-        var previewNode = document.querySelector("#template");
-        previewNode.id = "";
-        var previewTemplate = previewNode.parentNode.innerHTML;
-        previewNode.parentNode.removeChild(previewNode);
+    created: function () {
+        this.$nextTick(function () {
+            if (!this.url) {
+                return;
+            }
+            var previewNode = document.querySelector("#template");
+            previewNode.id = "";
+            var previewTemplate = previewNode.parentNode.innerHTML;
+            previewNode.parentNode.removeChild(previewNode);
 
-        this.original = this.current;
+            this.original = this.current;
 
-        this._dropzone = $(this.$el).dropzone({
-            url: this.url,
-            previewsContainer: '#previews',
-            previewTemplate: previewTemplate,
-            clickable: $(this.$el).parent().find('.select-files').get()[0],
-            maxFilesize: 8,
-            success: function (file, data) {
-                data = JSON.parse(data);
+            this._dropzone = $(this.$el).dropzone({
+                url: this.url,
+                previewsContainer: '#previews',
+                previewTemplate: previewTemplate,
+                clickable: $(this.$el).parent().find('.select-files').get()[0],
+                maxFilesize: 8,
+                success: function (file, data) {
+                    data = JSON.parse(data);
 
-                if (data.success) {
-                    this.prev = this.current;
-                    this.current = data.url;
-                }
-            }.bind(this)
+                    if (data.success) {
+                        this.prev = this.current;
+                        this.current = data.url;
+                    }
+                }.bind(this)
+            });
         });
     }
 });
