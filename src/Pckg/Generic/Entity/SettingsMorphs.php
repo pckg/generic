df6fb6a4c7c13f96f4a75e3f8a7ab1e68662da12
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-03-22 23:15:50 +0100

    Remove dynamic

--- src/Pckg/Generic/Entity/SettingsMorphs.php
+++ src/Pckg/Generic/Entity/SettingsMorphs.php
@@ -9,8 +9,6 @@ class SettingsMorphs extends Entity
 
     protected $record = SettingsMorph::class;
 
-    protected $repository = Repository::class . '.dynamic';
-
     public function setting()
     {
         return $this->belongsTo(Settings::class)
