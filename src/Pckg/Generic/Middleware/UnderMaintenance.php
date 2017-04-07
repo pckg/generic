be4899bef23ce01c4b9b2af822a48d6d195f5cc1
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-04-07 14:06:29 +0200

    Maintenance

---
+++ src/Pckg/Generic/Middleware/UnderMaintenance.php
@@ -0,0 +1,15 @@
+<?php namespace Pckg\Generic\Middleware;
+
+class UnderMaintenance
+{
+
+    public function execute(callable $next)
+    {
+        if (config('maintenance') && !dev()) {
+            response()->respond(view('Pckg/Generic:maintenance')->autoparse());
+        }
+
+        return $next();
+    }
+
+}
