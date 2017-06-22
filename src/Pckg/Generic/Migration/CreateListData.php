2d99bf286e8b602eae1ed524b9378631112d3eac
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-06-22 18:04:17 +0200

    Cleanuš to config

--- src/Pckg/Generic/Migration/CreateListData.php
+++ src/Pckg/Generic/Migration/CreateListData.php
@@ -11,53 +11,7 @@ class CreateListData extends Migration
 
     public function up()
     {
-        $lists = [
-            [
-                'id'    => 'actionsMorphs.backgrounds',
-                'title' => 'Action backgrounds',
-                'items' => [
-                    'primary'            => 'Primary',
-                    'primary-inverted'   => 'Inverted primary',
-                    'secondary'          => 'Secondary',
-                    'secondary-inverted' => 'Inverted secondary',
-                ],
-            ],
-            [
-                'id'    => 'actionsMorphs.widths',
-                'title' => 'Action widths',
-                'items' => [
-                    'fluid'     => 'Fluid',
-                    'small'     => 'Small',
-                    'medium'    => 'Medium',
-                    'large'     => 'Large',
-                    'col-md-3'  => 'Quarter (col-md-3)',
-                    'col-md-4'  => 'Third (col-md-4)',
-                    'col-md-6'  => 'Half (col-md-6)',
-                    'col-md-8'  => 'Two thirds (col-md-8)',
-                    'col-md-12' => 'Full (col-md-12)',
-                ],
-            ],
-            [
-                'id'    => 'mails.types',
-                'title' => 'Mail types',
-                'items' => [
-                    'frontend'     => 'Frontend / Transactional',
-                    'notification' => 'Notification / Event',
-                    'newsletter'   => 'Newsletter',
-                    'custom'       => 'Custom',
-                ],
-            ],
-            [
-                'id'    => 'feedbacks.statuses',
-                'title' => 'Feedback statuses',
-                'items' => [
-                    'opened'    => 'Opened',
-                    'resolving' => 'Resolving',
-                    'closed'    => 'Closed',
-                    'resolved'  => 'Resolved',
-                ],
-            ],
-        ];
+        $lists = config('pckg.generic.lists', []);
 
         foreach ($lists as $listConfig) {
             $list = (new Lists())->where('slug', $listConfig['id'])->one();
