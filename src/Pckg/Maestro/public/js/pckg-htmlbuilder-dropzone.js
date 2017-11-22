55ed7119669e6cd0d067f29b288a44fbffd7ea32
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-11-22 18:42:18 +0100

    Dont parse

--- src/Pckg/Maestro/public/js/pckg-htmlbuilder-dropzone.js
+++ src/Pckg/Maestro/public/js/pckg-htmlbuilder-dropzone.js
@@ -71,8 +71,6 @@ var pckgTabelizeFieldEditor = Vue.component('pckg-htmlbuilder-dropzone', {
                 clickable: $(this.$el).parent().find('.select-files').get()[0],
                 maxFilesize: 8,
                 success: function (file, data) {
-                    data = JSON.parse(data);
-
                     if (data.success) {
                         this.prev = this.current;
                         this.current = data.url;
