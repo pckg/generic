01452ca002bccdf74ef99a6a11e7b48b377edd81
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-01-27 14:34:25 +0100

    Pckg clipboard

---
+++ src/Pckg/Generic/View/pckgClipboard.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="input-group pckg-clipboard">
+        <a v-if="type == 'default' && link" class="input-group-addon" :href="text" title="Open in new tab"><i class="fa fa-link"></i></a>
+        <input type="text" class="form-control"
+               @click.prevent="copyToClipboard"
+               data-toggle="tooltip" v-model="text"/>
+        <a target="_blank" @click.prevent="copyToClipboard" class="input-group-addon" :href="text" title="Copy link"><i class="fa fa-copy"></i></a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'pckg-clipboard',
+        mixins: [pckgDelimiters],
+        props: {
+            type: {
+                type: String,
+                default: 'default'
+            },
+            text: {
+                type: String,
+                default: ''
+            },
+            link: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data: function () {
+            return {
+                error: ''
+            };
+        },
+        methods: {
+            copyToClipboard: function () {
+                var element = $(this.$el).find('input').get(0);
+                var title = 'Please press Ctrl/Cmd+C to copy';
+
+                // is element selectable?
+                if (element && element.select) {
+                    element.select();
+
+                    try {
+                        document.execCommand('copy');
+                        //element.blur();
+                        title = 'Copied!';
+                    } catch (err) {
+                        console.log(err);
+                    }
+
+                }
+
+                $(element).tooltip({
+                    'trigger': 'manual',
+                    'title': title
+                });
+
+                $(element).tooltip('show').mouseout(function () {
+                    $(element).tooltip('hide');
+                });
+            }
+        }
+    }
+</script>
