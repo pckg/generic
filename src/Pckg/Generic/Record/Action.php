9f18af8804091ea95e7927471ae9081fb79b5c71
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-01-30 14:28:26 +0100

    Revert

--- src/Pckg/Generic/Record/Action.php
+++ src/Pckg/Generic/Record/Action.php
@@ -64,30 +64,26 @@ class Action extends Record
                 'pckg.generic.pageStructure.bgImage'      => 'background-image',
                 'pckg.generic.pageStructure.margin'       => 'margin', // @deprecated
                 'pckg.generic.pageStructure.padding'      => 'padding', // @deprecated
-            ],
-            [
-                'pckg.generic.pageStructure.style' => 'style',
+                'pckg.generic.pageStructure.style'        => 'style',
             ],
         ];
 
         $settings = $this->pivot->settings;
         $styles = [];
-        foreach ($settings as $ss) {
-            foreach ($ss as $setting) {
-                if (!array_key_exists($setting->slug, $mapper)) {
-                    continue;
-                }
-
-                if ($setting->slug == 'pckg.generic.pageStructure.style') {
-                    $value = $setting->pivot->value . ';';
-                } else if ($setting->slug == 'pckg.generic.pageStructure.bgImage') {
-                    $value = $mapper[$setting->slug] . ': url(' . cdn('/storage/uploads/' . $setting->pivot->value) .
-                             ')';
-                } else {
-                    $value = $mapper[$setting->slug] . ': ' . $setting->pivot->value;
-                }
-                $styles[] = $value;
+        foreach ($settings as $setting) {
+            if (!array_key_exists($setting->slug, $mapper)) {
+                continue;
             }
+
+            if ($setting->slug == 'pckg.generic.pageStructure.style') {
+                $value = $setting->pivot->value . ';';
+            } else if ($setting->slug == 'pckg.generic.pageStructure.bgImage') {
+                $value = $mapper[$setting->slug] . ': url(' . cdn('/storage/uploads/' . $setting->pivot->value) .
+                         ')';
+            } else {
+                $value = $mapper[$setting->slug] . ': ' . $setting->pivot->value;
+            }
+            $styles[] = $value;
         }
 
         return implode('; ', $styles);
