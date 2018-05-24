113c7f128bd1d116d7a61d31b443dfb4f56bcde4
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-05-24 22:50:59 +0200

    Remove dismissable

--- src/Pckg/Maestro/public/vue/pckg-bootstrap-modal.vue
+++ src/Pckg/Maestro/public/vue/pckg-bootstrap-modal.vue
@@ -3,7 +3,7 @@
         <div class="modal-dialog" :class="[size ? 'modal-' + size : '']">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button v-if="dismissable" type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeModal"><span
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeModal"><span
                             aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title">
                         <slot name="header"></slot>
