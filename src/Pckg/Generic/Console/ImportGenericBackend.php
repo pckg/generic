29e05e1153459208b046be2affc6386b06d48c56
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-06-20 16:03:15 +0200

    FIx

--- src/Pckg/Generic/Console/ImportGenericBackend.php
+++ src/Pckg/Generic/Console/ImportGenericBackend.php
@@ -4,17 +4,14 @@ use Pckg\Collection;
 use Pckg\Framework\Console\Command;
 use Pckg\Generic\Entity\ListItems;
 use Pckg\Generic\Entity\Lists;
-use Pckg\Generic\Entity\Routes;
 use Pckg\Generic\Record\Action;
-use Pckg\Generic\Record\ActionsMorph;
-use Pckg\Generic\Record\Content as ContentRecord;
 use Pckg\Generic\Record\Layout;
 use Pckg\Generic\Record\ListItem;
 use Pckg\Generic\Record\ListRecord;
 use Pckg\Generic\Record\Menu;
-use Pckg\Generic\Record\Route;
 use Pckg\Generic\Record\SettingType;
 use Pckg\Generic\Record\Variable;
+use Symfony\Component\Console\Input\InputOption;
 
 class ImportGenericBackend extends Command
 {
@@ -22,14 +19,15 @@ class ImportGenericBackend extends Command
     protected function configure()
     {
         $this->setName('generic:import-backend')
-             ->setDescription('Import actions, routes, variables, lists, items, ...');
+             ->setDescription('Import actions, routes, variables, lists, items, ...')
+             ->addOptions([
+                              'do' => 'Manually select items to import',
+                          ],
+                          InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL);
     }
 
-    public function handle()
+    public function importGenericList()
     {
-        /**
-         * Import generic lists and items.
-         */
         $lists = config('pckg.generic.lists', []);
         foreach ($lists as $listConfig) {
             $skipWhenExisting = $listConfig['skipWhenExisting'] ?? false;
@@ -47,9 +45,7 @@ class ImportGenericBackend extends Command
             }
 
             foreach ($listConfig['items'] as $key => $title) {
-                $listItem = (new ListItems())->where('list_id', $list->id)
-                                             ->where('slug', $key)
-                                             ->one();
+                $listItem = (new ListItems())->where('list_id', $list->id)->where('slug', $key)->one();
                 $deleted = strpos($title, '**deleted**') === 0;
 
                 if (!$listItem && !$deleted) {
@@ -59,7 +55,7 @@ class ImportGenericBackend extends Command
                                          'slug'    => $key,
                                          'value'   => $title,
                                      ]);
-                } else if ($listItem) {
+                } elseif ($listItem) {
                     if ($deleted) {
                         $this->output('Deleting item ' . $listConfig['id'] . '.' . $key);
                         $listItem->delete();
@@ -69,26 +65,27 @@ class ImportGenericBackend extends Command
                 }
             }
         }
+    }
 
-        /**
-         * Import layouts.
-         */
+    public function importLayouts()
+    {
+
         (new Collection(config('pckg.generic.layouts', [])))->each(function($template, $slug) {
             $layout = Layout::getOrNew(['slug' => $slug]);
 
             $layout->setAndSave(['template' => $template]);
         });
+    }
 
-        /**
-         * Import variables.
-         */
+    public function importVariables()
+    {
         (new Collection(config('pckg.generic.variables', [])))->each(function($name, $slug) {
             Variable::getOrCreate(['slug' => $slug]);
         });
+    }
 
-        /**
-         * Import menus.
-         */
+    public function importMenus()
+    {
         (new Collection([['slug' => 'frontend', 'template' => 'frontendMainNav']]))->each(function($menu, $slug) {
             $menuR = Menu::getOrNew(['slug' => $menu['slug']]);
 
@@ -96,22 +93,73 @@ class ImportGenericBackend extends Command
                 $menuR->setAndSave(['template' => $menu['template']]);
             }
         });
+    }
 
-        /**
-         * Import setting types.
-         */
+    public function importSettingTypes()
+    {
         (new Collection([['slug' => 'array']]))->each(function($settingType, $slug) {
             SettingType::getOrCreate(['slug' => $settingType['slug']]);
         });
+    }
 
-        /**
-         * Import actions.
-         */
+    public function importActions()
+    {
         (new Collection(config('pckg.generic.actions', [])))->each(function($action, $slug) {
             $actionRecord = Action::getOrNew(['slug' => $slug]);
 
             $actionRecord->setAndSave($action);
         });
+    }
+
+    public function handle()
+    {
+        /**
+         * Check if we should import selectively.
+         */
+        $dos = $this->option('do');
+        dd($dos);
+
+        /**
+         * Import generic lists and items.
+         */
+        if (!$dos || in_array('lists', $dos)) {
+            $this->importGenericList();
+        }
+
+        /**
+         * Import layouts.
+         */
+        if (!$dos || in_array('layouts', $dos)) {
+            $this->importLayouts();
+        }
+
+        /**
+         * Import variables.
+         */
+        if (!$dos || in_array('variables', $dos)) {
+            $this->importVariables();
+        }
+
+        /**
+         * Import menus.
+         */
+        if (!$dos || in_array('menus', $dos)) {
+            $this->importMenus();
+        }
+
+        /**
+         * Import setting types.
+         */
+        if (!$dos || in_array('settingTypes', $dos)) {
+            $this->importSettingTypes();
+        }
+
+        /**
+         * Import actions.
+         */
+        if (!$dos || in_array('actions', $dos)) {
+            $this->importActions();
+        }
 
         $this->output('Done');
     }
