0840a311742be7150014b93d245a6df1a26adb83
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-09-29 20:29:37 +0200

    Generic payment

--- src/Pckg/Generic/public/vue/pckg-generic-app-top.js
+++ src/Pckg/Generic/public/vue/pckg-generic-app-top.js
@@ -20,7 +20,7 @@ var pckgPlatformSettings = {
     },
     methods: {
         submitForm: function () {
-            this.validateAndSubmit(function(){
+            this.validateAndSubmit(function () {
                 http.post($(this.$el).find('form').attr('action'), this.form, function (data) {
                     $dispatcher.$emit('notification:' + (data.success ? 'success' : 'error'), data.message || (data.success ? 'Settings saved' : 'General error'));
                 }, function (response) {
@@ -41,7 +41,7 @@ var pckgPlatformSettings = {
 
 var pckgCdn = {
     methods: {
-        cdn: function(file) {
+        cdn: function (file) {
             if (!file) {
                 return file;
             }
@@ -68,12 +68,91 @@ var pckgTranslations = {
                 return translation;
             }
 
-            $.each(data, function(key, val){
+            $.each(data, function (key, val) {
                 translation = translation.replace('{{ ' + key + ' }}', val);
             });
 
             return translation;
         }
+    }
+};
+
+var pckgPayment = {
+    mixins: [pckgTranslations],
+    props: {
+        instalments: {
+            type: Array
+        },
+        handler: {
+            type: String
+        }
+    },
+    data: function () {
+        return {
+            formAction: '',
+            state: null
+        };
+    },
+    methods: {
+        handleSuccessResponse: function (data) {
+            var t = this;
+            if (data.redirect) {
+                t.state = 'redirected';
+                $.magnificPopup.open({
+                    items: {
+                        src: data.redirect,
+                        type: 'iframe'
+                    }
+                });
+                $.magnificPopup.instance.close = function () {
+                    if (!confirm("Do you want to cancel payment?")) {
+                        return;
+                    }
+
+                    t.state = 'canceled';
+                    $dispatcher.$emit('payment-form:canceled');
+                    $.magnificPopup.proto.close.call(this);
+                };
+            } else if (data.modal) {
+                t.state = data.modal;
+                $dispatcher.$emit('payment-form:' + data.modal, data.message);
+            } else if (!data.success) {
+                t.state = 'error';
+                $dispatcher.$emit('payment-form:error', data.message);
+            }
+        },
+        handleErrorResponse: function (data) {
+            this.state = 'error';
+            $dispatcher.$emit('payment-form:error', 'Unknown error');
+        },
+        submitForm: function (data) {
+            this.state = 'validating';
+            $dispatcher.$emit('payment-form:validating');
+            http.post(this.formAction, this.collectFormData(data), pckgPayment.methods.handleSuccessResponse, pckgPayment.methods.handleErrorResponse);
+        },
+        collectFormData: function () {
+            return {};
+        },
+        preFetch: {},
+        initialFetch: function () {
+            this.state = 'fetching';
+            http.post(utils.url('@api.payment.init', { handler: this.handler }), {
+                instalments: this.instalments.map(function (instalment) {
+                    return instalment.id;
+                })
+            }, function (data) {
+                this.formAction = data.formAction;
+
+                this.afterFetch(data);
+                this.state = 'fetched';
+            }.bind(this));
+        },
+        afterFetch: function (data) {
+        }
+    },
+    created: function(){
+        this.preFetch();
+        this.initialFetch();
     }
 };
 
