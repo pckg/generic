6d81336a08400292c4e7727690ce0c611d885e53
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-11-15 17:15:39 +0100

    Data attributes

---
+++ src/Pckg/Generic/Entity/DataAttributes.php
@@ -0,0 +1,11 @@
+<?php namespace Pckg\Generic\Entity;
+
+use Pckg\Database\Entity;
+use Pckg\Generic\Record\DataAttribute;
+
+class DataAttributes extends Entity
+{
+
+    protected $record = DataAttribute::class;
+
+}
---
+++ src/Pckg/Generic/Record/DataAttribute.php
@@ -0,0 +1,11 @@
+<?php namespace Pckg\Generic\Record;
+
+use Pckg\Database\Record;
+use Pckg\Generic\Entity\DataAttributes;
+
+class DataAttribute extends Record
+{
+
+    protected $entity = DataAttributes::class;
+
+}
