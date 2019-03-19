df36da41f118433de29512c9538f751545e5ba34
Nemanja Glumac <31325167+nemanjaglumac@users.noreply.github.com>
2019-03-19 17:45:15 +0100

    [f-21] Improve notifications with animations (#13)
    * Improve notifications with animations
    
    Added animations from Animate.css package:
    1. danger => shake
    2. info || success => fadeInUp
    
    * Refactor component's class
    
    Resolves: https://github.com/pckg/generic/pull/13#pullrequestreview-216221487

--- src/Pckg/Generic/View/dispatcherNotifications.vue
+++ src/Pckg/Generic/View/dispatcherNotifications.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="pckg-dispatcher-notifications-wrapper">
-        <div class="alert alert-dismissible alert-fixxed" role="alert"
-             :class="'alert-' + notification.type"
-             v-for="notification in notifications">
+        <div 
+            class="alert alert-dismissible animated" role="alert"
+            :class="alertClass(notification)"
+            v-for="notification in notifications">
 
             <div class="col">
                 <i class="fas fa-fw" :class="iconClass(notification)"></i>
@@ -30,6 +31,11 @@
         methods: {
             removeNotification: function (notification) {
                 utils.splice(this.notifications, notification);
+            },
+            alertClass: function(notification) {
+                let alertType = 'alert-' + notification.type;
+                let animationType = notification.type === 'danger' ? 'shake' : 'fadeInUp';
+                return `${alertType} ${animationType}`;
             },
             iconClass: function (notification) {
                 return notification.type == 'danger'
