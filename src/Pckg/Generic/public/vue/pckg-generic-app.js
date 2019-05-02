181a4af2be27cae0ead43e272c47c82fc8f2c22a
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-05-02 23:31:28 +0200

    Abstract header

--- src/Pckg/Generic/public/vue/pckg-generic-app.js
+++ src/Pckg/Generic/public/vue/pckg-generic-app.js
@@ -115,9 +115,9 @@ const $store = new Vuex.Store({
     modules: Pckg.vue.stores,
 });
 
-if ($('header.c-header').length > 0) {
+if ($('header.a-header').length > 0) {
     new Vue({
-        el: 'header.c-header',
+        el: 'header.a-header',
         $store,
         router,
         computed: {
