ccc49626ec6a5dab9a6e0ea77c12d05c08ec7f42
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-08-21 14:14:50 +0200

    Vimeo background

--- src/Pckg/Generic/View/vue/bg.vue
+++ src/Pckg/Generic/View/vue/bg.vue
@@ -1,10 +1,10 @@
 <template>
-    <div v-if="display == 'background' && identifier && source == 'youtube'" class="video-background">
+    <div v-if="display == 'background' && identifier" class="video-background">
         <div class="video-foreground">
             <iframe :src="url" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
-    <a v-else-if="display == 'popup' && identifier && source == 'youtube'" :href="url" class="popup-iframe"></a>
+    <a v-else-if="display == 'popup' && identifier" :href="url" class="popup-iframe"></a>
 </template>
 
 <script>
@@ -51,6 +51,18 @@
                             + '&playsinline=1&rel=0&showinfo=0&playlist=' + this.identifier;
                     } else {
                         return 'https://www.youtube-nocookie.com/watch?v=' + this.identifier;
+                    }
+                } else if (this.source == 'vimeo') {
+                    if (this.display == 'background') {
+                        return 'https://player.vimeo.com/video/' + this.identifier
+                            + '?controls=' + (this.action.settings.bgVideoControls == 'yes' ? 'true' : 'false')
+                            + '&autoplay=' + (this.action.settings.bgVideoAutoplay == 'yes' ? 'true' : 'false')
+                            + '&loop=' + (this.action.settings.bgVideoLoop == 'yes' ? 'true' : 'false')
+                            + '&byline=' + (this.action.settings.bgVideoBranding == 'yes' ? 'true' : 'false')
+                            + (this.action.settings.bgVideoMute ? '&muted=true' : '')
+                            + '&dnt=true&playsinline=true&portrait=false&responsive=true&transparent=true';
+                    } else {
+                        return 'https://player.vimeo.com/video/' + this.identifier;
                     }
                 }
             }
