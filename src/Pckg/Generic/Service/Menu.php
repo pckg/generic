9d7f0000ae9067699c389bf8193f863636ab2141
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-01-22 20:42:14 +0100

    Allow returning of only menu items

--- src/Pckg/Generic/Service/Menu.php
+++ src/Pckg/Generic/Service/Menu.php
@@ -44,6 +44,9 @@ class Menu
         if (is_string($slug)) {
             trigger(Menu::class . '.collectMenuItems.' . $slug, $menuItems);
         }
+        if (isset($params['returnMenuItems'])) {
+            return $this->buildTree($menuItems);
+        }
 
         return view(
             'Pckg\Generic:menu\\' . $menus->first()->template,
